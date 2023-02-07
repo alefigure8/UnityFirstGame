432842369a684f26aaba923ce3b88df8e86df018
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2023-02-07 18:17:21 -0300

    First commit

---
+++ Assets/_Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float speed = 5f;
+
+    private void Update()
+    {
+        Vector2 inputVector = new Vector2(0, 0);
+        
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector.y += 1;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x -= 1;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y -= 1;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x += 1;
+        }
+
+        inputVector = inputVector.normalized;
+        Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
+        transform.position += moveDir * Time.deltaTime * speed;
+        
+        Debug.Log(inputVector);
+    }
+}
