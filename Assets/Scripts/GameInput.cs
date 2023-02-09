3cd5add3bfa55fa1127d3999558aeb8aec88b33b
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2023-02-09 10:49:58 -0300

    Add new input Actions

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -8,13 +8,13 @@ public class GameInput : MonoBehaviour
     
     private void Awake()
     {
-        playerInputActions  = new PlayerInputActions();
+        playerInputActions = new PlayerInputActions();
         playerInputActions.Player.Enable();
     }
     
     public Vector2 GetMovementVectorNormalized()
     {
-        Vector2 inputVector = playerInputActions.Player.Movement.ReadValue<Vector2>();
+        Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
 
         // Normalize the input vector so that the player moves at the same speed diagonally as they do horizontally or vertically
         inputVector = inputVector.normalized;
