907a3943688e13747347c0965ad384fb29bd8fb7
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2023-02-09 17:26:57 -0300

    Add evento cuando se presionar un botón

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -1,17 +1,28 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameInput : MonoBehaviour
 {
+    public event EventHandler OnInteractAction;
     private PlayerInputActions playerInputActions;
     
     private void Awake()
     {
+        // InputActions
         playerInputActions = new PlayerInputActions();
         playerInputActions.Player.Enable();
+
+        // Subscribe to the Interact action
+        playerInputActions.Player.Interact.performed += Interact_performed;
     }
     
+    private void Interact_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        OnInteractAction?.Invoke(this, EventArgs.Empty);
+    }
+
     public Vector2 GetMovementVectorNormalized()
     {
         Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
