631201958bcf2ff61c4af2383b819d72aca5e83d
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2023-02-14 17:56:11 -0300

    Init BaseContainer

---
+++ Assets/Scripts/BaseCounter.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BaseCounter : MonoBehaviour, IKitchenObjectParent
+{
+    [SerializeField] private Transform counterTopPoint;
+
+    private kitchenObject kitchenObject;
+    
+    public virtual void Interact(Player player)
+    {
+        Debug.LogError("Nevev Invoke");
+    }
+
+    public Transform getKitchenObjectFollowTransform()
+    {
+        return counterTopPoint;
+    }
+
+    public void SetKitchenObject(kitchenObject kitchenObject)
+    {
+        this.kitchenObject = kitchenObject;
+    }
+
+    public kitchenObject GetKitchenObject()
+    {
+        return kitchenObject;
+    }
+
+    public void ClearKitchenObject()
+    {
+        this.kitchenObject = null;
+    }
+
+    public bool HasKitchenObject()
+    {
+        return kitchenObject != null;
+    }
+}
