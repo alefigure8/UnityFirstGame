214b3dd4abbf6a930b49830b954b726dd114e140
Ale Gomez Nieto <alejandro.gomez@alumnos.frgp.utn.edu.ar>
2023-02-23 17:45:53 -0300

    Add evento que dispara animacion

---
+++ Assets/Scripts/CuttingCounterVisual.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class CuttingCounterVisual : MonoBehaviour
+    {
+        private Animator animator;
+        private const string CUT = "Cut";
+
+        [SerializeField] CuttingCounter cuttingCounter;
+
+        private void Awake()
+        {
+            animator = GetComponent<Animator>();
+        }
+
+        private void Start()
+        {
+            cuttingCounter.OnCut += CuttingCounter_OnCut;
+        }
+
+        private void CuttingCounter_OnCut(object sender, EventArgs e)
+        {
+            animator.SetTrigger(CUT);
+        }
+    }
+}
